15003e3c0db53c19a21c5d67fd7419c8bcd27f98
Augusto Ayala <50784512+AugustoAyala@users.noreply.github.com>
2020-05-18 20:07:31 -0300

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const users = require('../controllers/auth-controller');
 const router = express.Router();
 
-
 router.post('/api/register', users.create);
 router.post('/api/login', users.login);
 
